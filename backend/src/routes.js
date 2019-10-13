69c2fcf62e421a3573e9028e5595c23fdcf6b9e5
Jonas da Silva Fragoso <jonas.fragoso@dbserver.com.br>
2019-10-13 01:39:49 -0300

    Addind new routes and controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,9 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashBoardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
+
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -18,5 +21,7 @@ routes.get('/spots',  SpotController.index);
 routes.get('/dashboard',  DashBoardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store)
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
