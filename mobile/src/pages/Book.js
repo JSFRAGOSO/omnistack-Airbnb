2028053fb08b62fd4e806022a9c8b259201ab988
Jonas da Silva Fragoso <jonas.fragoso@dbserver.com.br>
2019-10-13 00:59:36 -0300

    App done

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {View,Alert,AsyncStorage,Text,TextInput,TouchableOpacity} from 'react-native';
+import {View,Alert,AsyncStorage,Text,TextInput,TouchableOpacity,StyleSheet} from 'react-native';
 
 import api from '../services/api';
 
@@ -10,16 +10,15 @@ export default function Book({navigation}){
     const spot_id = navigation.getParam('id');
 
     async function handleSubmit(){
-            
-        await api.post('/spots/:spot_id/bookings', {
+        //console.log(user_id);
+        const response = await api.post(`/spots/${spot_id}/bookings`, {
             date,
-            params:{
-                spot_id
-            },
             headers:{
                 user_id
             }            
         });
+
+        console.log(response);
 
         Alert.alert('Solicitação de reserva enviada');
         navigation.navigate('List');
@@ -30,9 +29,10 @@ export default function Book({navigation}){
     }
 
     return (
-        <View>
-            <Text> DATA DE INTERESSE * </Text>
+        <View style = {styles.containter}>
+            <Text style = {styles.label}> DATA DE INTERESSE * </Text>
             <TextInput
+                style = {styles.input}
                 placeholder = "Qual data você quer reservar?"
                 placeholderTextColor = "#999"
                 autoCapitalize = "words"
@@ -40,12 +40,52 @@ export default function Book({navigation}){
                 value = {date}
                 onChangeText = {setDate}
             />
-            <TouchableOpacity onPress = {handleSubmit}>
-                <Text>Solicitar Reserva</Text>
+            <TouchableOpacity style = {styles.button} onPress = {handleSubmit}>
+                <Text style = {styles.buttonText}>Solicitar Reserva</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress = {handleCancel}>
-                <Text>Cancelar Reserva</Text>
+            <TouchableOpacity style = {[styles.button,styles.cancelButton]} onPress = {handleCancel}>
+                <Text style = {styles.buttonText}>Cancelar Reserva</Text>
             </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    containter:{
+        margin:30
+    },
+    label:{
+        fontWeight:'bold',
+        color:'#444',
+        marginBottom:8,
+        marginTop:30
+    },
+    input:{
+        borderWidth:1,
+        borderColor:'#ddd',
+        paddingHorizontal:20,
+        fontSize:16,
+        color:'#444',
+        height:44,
+        marginBottom:20,
+        borderRadius:2
+    },
+    button:{
+        height:42,
+        backgroundColor:'#f05a5b',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:2,
+        marginVertical:10,
+    },
+    cancelButton: {
+        backgroundColor: '#ccc',
+        marginTop: 10
+    },
+    buttonText:{
+        color:'#fff',
+        fontWeight:'bold',
+        fontSize:16
+    }
+
+})
